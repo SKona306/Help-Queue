185422153bc408073700f85209d4559117c08f55
Shaun Kent <shaunkent81@gmail.com>
2022-01-23 11:33:42 -0800

    add tests to check update time action creator

--- src/__tests__/reducers/ticket-list-reducer.test.js
+++ src/__tests__/reducers/ticket-list-reducer.test.js
@@ -1,5 +1,6 @@
 import ticketListReducer from '../../reducer/ticket-list-reducer';
 import * as c from '../../actions/ActionTypes';
+import Moment from 'moment';
 
 describe('tickietListReducer', () => {
 
@@ -23,8 +24,51 @@ describe('tickietListReducer', () => {
       names: 'Ryan and Aimen',
       location: '5c',
       issue:'redux action is not working correctly',
+      timeOpen: 0,
       id: 1
-    }
+    };
+
+    test('Should successfully add a ticket to the ticket list that includes Moment-formatted wait times', () => {
+      const { names, location, issue, timeOpen, id } = ticketData;
+      action = {
+        type: c.ADD_TICKET,
+        names: names,
+        location: location,
+        issue: issue,
+        timeOpen: timeOpen,
+        id: id,
+        formattedWaitTime: new Moment().fromNow(true)
+      };
+      expect(ticketListReducer({}, action)).toEqual({
+        [id] : {
+          names: names,
+          location: location,
+          issue: issue,
+          timeOpen: timeOpen,
+          id: id,
+          formattedWaitTime: 'a few seconds'
+        }
+      });
+    });
+
+    test('Should add a formatted wait time to ticket entry', () => {
+      const { names, location, issue, timeOpen, id } = ticketData;
+      action = {
+        type: c.UPDATE_TIME,
+        formattedWaitTime: '4 minutes',
+        id: id
+      };
+      expect(ticketListReducer({ [id] : ticketData}, action)).toEqual({
+        [id] : {
+          names: names,
+          location: location,
+          issue: issue,
+          timeOpen: timeOpen,
+          id: id,
+          formattedWaitTime: '4 minutes'
+        }
+      });
+    });
 
     test('Should successfully delete a ticket', () => {
       action = {
@@ -41,25 +85,25 @@ describe('tickietListReducer', () => {
       });
     });
 
-  test('Should successfully add new ticket data to mainTicketList', () => {
-    const { names, location, issue, id } = ticketData;
-    action = {
-      type: c.ADD_TICKET,
-      names: names,
-      location: location,
-      issue: issue,
-      id: id
-    }
+  // test('Should successfully add new ticket data to mainTicketList', () => {
+  //   const { names, location, issue, id } = ticketData;
+  //   action = {
+  //     type: c.ADD_TICKET,
+  //     names: names,
+  //     location: location,
+  //     issue: issue,
+  //     id: id
+  //   }
 
-    expect(ticketListReducer({}, action)).toEqual({
-      [id] : {
-        names:names,
-        location: location,
-        issue: issue,
-        id: id
-      }
-    });
-  });
+  //   expect(ticketListReducer({}, action)).toEqual({
+  //     [id] : {
+  //       names:names,
+  //       location: location,
+  //       issue: issue,
+  //       id: id
+  //     }
+  //   });
+  // });
 
   test('Should return default state if there is no action type pased into the reducer', () => {
     expect(ticketListReducer({}, {type:null })).toEqual({});
