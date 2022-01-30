1e60704c133711a05f189e0b29a1bed1d55e2380
Shaun Kent <shaunkent81@gmail.com>
2022-01-30 15:19:58 -0800

    change firebase version, configure newTicketForm to add to firebase db

--- src/components/NewTicketForm.js
+++ src/components/NewTicketForm.js
@@ -1,22 +1,33 @@
 import React from "react";
-import { v4 } from "uuid";
 import PropTypes from "prop-types";
 import ReusableForm from "./ReusableForm";
-import Moment from "moment";
+import { useFirestore } from 'react-redux-firebase';
 
 function NewTicketForm(props){
-  return(
+
+  const firestore = useFirestore();
+
+  function addTicketToFirestore(event) {
+    event.preventDefault();
+    props.onNewTicketCreation();
+
+    return firestore.collection('tickets').add(
+      {
+        names: event.target.names.value,
+        location: event.target.location.value, 
+        issue: event.target.issue.value,
+        timeOpen: firestore.FieldValue.serverTimestamp()
+      }
+    );
+  }
+
+  return (
     <React.Fragment>
-      <ReusableForm
-        formSubmissionHandler={handleNewTicketFormSubmission}
+      <ReusableForm 
+        formSubmissionHandler={addTicketToFirestore}
         buttonText="Help!" />
     </React.Fragment>
   );
-  function handleNewTicketFormSubmission(event) {
-    event.preventDefault();
-    props.onNewTicketCreation({names: event.target.names.value, location: event.target.location.value,
-    issue: event.target.issue.value, id: v4(), timeOpen: new Moment(), formattedWaitTime: new Moment().fromNow(true) });
-  }
 }
 
 NewTicketForm.propTypes = {
